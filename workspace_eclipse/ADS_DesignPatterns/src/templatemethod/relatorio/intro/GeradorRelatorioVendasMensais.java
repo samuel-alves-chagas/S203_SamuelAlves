a166e62a6ef051eb2ddee560f5302bfdb95b6d3a
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2022-11-07 21:02:27 -0300

    Aula de padroes arquiteturais

---
+++ workspace_eclipse/ADS_DesignPatterns/src/templatemethod/relatorio/intro/GeradorRelatorioVendasMensais.java
@@ -0,0 +1,41 @@
+package templatemethod.relatorio.intro;
+
+import java.util.List;
+
+public class GeradorRelatorioVendasMensais {
+	
+	public String getCabecalho() {
+		String cabecalho = "Empresa My" + '\n' + "----------";
+		return cabecalho;
+	}
+	
+	private String getRodape() {
+		String rodape = "-----------------------" + '\n'+ "* Propriedade particular - Uso confidencial";
+		return rodape;
+	}
+
+	private List<String> getConteudo(Integer ano, Integer mes){
+		List<String> conteudo = ServicoDeRelatorio.getInstance().getVendasMensal(ano, mes);
+		
+		return conteudo;
+	}
+	
+	public String criarPagina(Integer ano, Integer mes) {
+		String cabecalho = getCabecalho();
+		List<String> conteudo = getConteudo(ano,mes);
+		String rodape = getRodape();
+		
+		StringBuffer bufferPagina = new StringBuffer();
+		bufferPagina.append(cabecalho);
+		
+		for(String linha : conteudo) {
+			bufferPagina.append('\n');
+			bufferPagina.append(linha);
+		}
+		
+		bufferPagina.append('\n');
+		bufferPagina.append(rodape);
+		
+		return bufferPagina.toString();
+	}
+}
